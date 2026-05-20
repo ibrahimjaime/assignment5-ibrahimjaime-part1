1d47ddda99a0dbc92cb94cf542cd5cd3be9f40b9
ibrahim.jaime <ibrahimjaime@gmail.com>
2026-05-20 20:34:10 -0300

    fix(finder-app): support PATH execution and result output

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat /home/conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 
 # make clean
 # make
@@ -35,9 +35,9 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat /home/conf/assignment.txt)
+assignment=$(cat /etc/finder-app/conf/assignment.txt)
 
-if [ $assignment != 'assignment1' ]
+if [ "$assignment" != "assignment1" ]
 then
 	mkdir -p "$WRITEDIR"
 
@@ -57,10 +57,12 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+
+finder.sh "$WRITEDIR" "$WRITESTR" > /tmp/assignment4-result.txt
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
